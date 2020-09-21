e81f842645d82e29e841bdf2a18e7da07097f51d
Luke Shay <lshay@sourceallies.com>
2020-09-20 20:49:55 -0500

    remove prints

--- main.c
+++ main.c
@@ -245,7 +245,6 @@ void parse(struct command *cmd, char *cmdline) {
 
     cmd->cmd[length - 2] = '\0';
   }
-  printf("1: %s\n", cmd->argv[0]);
 }
 
 /**
@@ -291,13 +290,11 @@ void run_system_command(struct command *cmd) {
   pid_t child_pid;
   int fd;
 
-  printf("4: %s\n", cmd->argv[0]);
   // Forks the current process and sees if it is successful
   if ((child_pid = fork()) < 0) {
     fprintf(stderr, "fork() error");
   } else if (child_pid == 0) { // Runs the command because the PID indicates it
 
-    printf("5: %s\n", cmd->argv[0]);
     // is the child process
     fd = redirect(
         cmd); // Calls redirect helper which redirects input/output if necessary
@@ -408,8 +405,6 @@ int eval(char *cmdline) {
   // Parses the command
   parse(&cmd, cmdline);
 
-  printf("2: %s\n", cmd.argv[0]);
-
   // If bg is -1 or the command is NULL, we return to get the next command
   if (cmd.bg == -1 || cmd.argv[0] == NULL) {
     ret = 1;
@@ -417,7 +412,6 @@ int eval(char *cmdline) {
   } else if (cmd.builtin ==
              (enum builtin_t)NONE) { // Runs the system command and returns
 
-    printf("3: %s\n", cmd.argv[0]);
     run_system_command(&cmd);
     ret = 1;
   } else { // Runs builtin command and returns it's return value
