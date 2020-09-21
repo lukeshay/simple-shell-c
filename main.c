1a9966cf4c1c21fefb3ef76dfb0a44f8a5d2dba8
Luke Shay <lshay@sourceallies.com>
2020-09-20 20:30:17 -0500

    prints

--- main.c
+++ main.c
@@ -301,9 +301,15 @@ void run_system_command(struct command *cmd) {
   } else if (child_pid == 0) { // Runs the command because the PID indicates it
 
     printf("5: %s\n", cmd->argv[0]);
+    printf("5: %s\n", cmd->argv[0]);
+    printf("5: %s\n", cmd->argv[0]);
+    printf("5: %s\n", cmd->argv[0]);
+    printf("5: %s\n", cmd->argv[0]);
+    printf("5: %s\n", cmd->argv[0]);
+    printf("5: %s\n", cmd->argv[0]);
     // is the child process
-    // fd = redirect(
-        // cmd); // Calls redirect helper which redirects input/output if necessary
+    fd = redirect(
+        cmd); // Calls redirect helper which redirects input/output if necessary
 
     if (fd == -1) {
       fprintf(stderr, "Error opening file '%s'", cmd->file);
